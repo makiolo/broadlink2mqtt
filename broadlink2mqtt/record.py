79cba5150d379b98a2da91b29112d0f0d1b71d94
Ricardo <makiolo@gmail.com>
2017-08-23 17:47:15 +0200

    Update record.py

--- broadlink2mqtt/record.py
+++ broadlink2mqtt/record.py
@@ -23,25 +23,24 @@ def execute_command(device, config, machine, command, postsleep=0):
 
 if __name__ == '__main__':
     device = get_broadlink_rm()
-    print "-- grabando secuencia"
+    print ("-- grabando secuencia")
     sequence = []
     while len(sequence) < 1:
-        print "/",
+        print ("/")
         device.enter_learning()
         time.sleep(5)
-        print "\\",
+        print ("\\")
         ir_packet = device.check_data()
         if ir_packet is not None:
             sequence.append( base64.b64encode(ir_packet) )
-            print "ok"
+            print ("ok")
 
     try:
         while True:
-            print "-- enviando secuencia"
+            print ("-- enviando secuencia")
             for ir_packet_encoded in sequence:
-                print "- send {}".format(ir_packet_encoded)
+                print ("- send {}".format(ir_packet_encoded))
                 device.send_data( base64.b64decode(ir_packet_encoded) )
             time.sleep(3)
     except KeyboardInterrupt:
         pass
-
